f8e4192c55e7a472a7b9750e12922830760764f8
prop4n <legrandenzo.pro@gmail.com>
2024-10-24 12:52:45 +0200

    eee

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is runneeeing on http://localhost:${port}`);
 });
