49fc92941d817eeb07b16b3fa6f0cae10b5db30c
prop4n <legrandenzo.pro@gmail.com>
2024-10-29 19:40:11 +0100

    eee

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/hello', (req, res) => {
-    res.send('Hello World! test');
+    res.send('Hello World! teste');
 });
 
 app.listen(port, () => {
